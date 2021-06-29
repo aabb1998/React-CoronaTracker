6532f0547142060be52ad59d37a3cc42eeaf170b
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2021-06-30 01:40:05 +1000

    created the header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "../../Assets/HeaderAssets/logo.svg";
+import phone from "../../Assets/HeaderAssets/phone.png";
+import menu from "../../Assets/HeaderAssets/icons8-menu-50.png";
+import "./Header.css";
+
+function Header() {
+	return (
+		<div className="header-container">
+			<div className="logo">
+				<img className="logo" src={logo} alt="logo" />
+			</div>
+			<div className="navigation">
+				<ul>
+					<li>
+						<a href="/">About</a>
+					</li>
+					<li>
+						<a href="/">Live Data</a>
+					</li>
+					<li>
+						<a href="/">Symptoms</a>
+					</li>
+					<li>
+						<a href="/">Prevention</a>
+					</li>
+				</ul>
+			</div>
+			<div className="emergency">
+				<div className="emergency-section">
+					<img className="emergency-phone" src={phone} alt="phone" />
+					<span>Emergency Contact</span>
+				</div>
+				<img className="menu-btn" src={menu} alt="menu" />
+			</div>
+		</div>
+	);
+}
+
+export default Header;
