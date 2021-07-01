13d1cdae89087803f20de5180609f82aeea09b83
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2021-07-02 05:16:02 +1000

    Added map

---
+++ src/Components/LiveData/DataMap.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+import reactDom from 'react-dom';
+import './Map.css';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from 'react-simple-maps';
+
+function DataMap({ setToolTipContent }) {
+  const geoUrl =
+    'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
+
+  return (
+    <>
+      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
+        <ZoomableGroup>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    const { NAME, POP_EST } = geo.properties;
+                    setToolTipContent(`${NAME} - ${POP_EST}`);
+                  }}
+                  onMouseLeave={() => {
+                    setToolTipContent('');
+                  }}
+                  style={{
+                    default: { fill: '#D6D6DA', outline: 'none' },
+                    hover: {
+                      fill: '#F53',
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#E42',
+                      outline: 'none',
+                    },
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
+  );
+}
+
+export default DataMap;
--- src/Components/LiveData/LiveData.js
+++ src/Components/LiveData/LiveData.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './LiveData.css';
+import reactDom from 'react-dom';
+import { useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import DataMap from './DataMap';
 
 function LiveData() {
+  const [content, setContent] = useState('');
+
+  const geoUrl =
+    'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
+
   return (
-    <div>
+    <div className="data-map">
       <h3>Live Data</h3>
+      <DataMap setToolTipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
