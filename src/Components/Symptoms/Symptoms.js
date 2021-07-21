189e1ca9e86625ee90a6d166b7e88598ffaf419a
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2021-07-21 21:02:09 +1000

    Symptoms page changes

--- src/Components/Symptoms/Symptoms.js
+++ src/Components/Symptoms/Symptoms.js
@@ -9,7 +9,10 @@ function Symptoms() {
 			<div className="main-symptom-section">
 				<div className="left-side">
 					<div className="text-section">
-						<h2>Keep you and your family safe.</h2>
+						<h2>
+							Keep you and your family{" "}
+							<span style={{ color: "red" }}>safe.</span>
+						</h2>
 						<h4>
 							Check for any covid-19 related symptoms below, and
 							contact emergency services if symptoms escalate.
@@ -29,6 +32,59 @@ function Symptoms() {
 				<div className="right-side">
 					<div className="main-img">
 						<img src={coronaWoman} alt="" />
+					</div>
+				</div>
+			</div>
+			<div className="symptoms-info-section">
+				<div className="main-symptom-content">
+					<div className="common-info">
+						<div className="img-header">
+							<img src="" alt="" />
+						</div>
+						<div className="common-info-text">
+							<h3>Common Symptoms:</h3>
+							<ul>
+								<li>Fever</li>
+								<li>Dry Cough</li>
+								<li>Fatigue</li>
+							</ul>
+						</div>
+					</div>
+					<div className="common-info">
+						<div className="img-header">
+							<img src="" alt="" />
+						</div>
+						<div className="common-info-text">
+							<h3>Other Symptoms:</h3>
+							<ul>
+								<li>Loss of taste or smell</li>
+								<li>Nasal Congestion</li>
+								<li>Conjunctivitis (also known as red eyes)</li>
+								<li>Sore throat</li>
+								<li>Headache</li>
+								<li>Muscle or joint pain</li>
+								<li>Different types of skin rash</li>
+								<li>Nausea or vomiting</li>
+								<li>Diarrhea, chills or dizzines</li>
+							</ul>
+						</div>
+					</div>
+					<div className="common-info-severe">
+						<div className="img-header">
+							<img src="" alt="" />
+						</div>
+						<div className="common-info-text">
+							<h3>Severe Symptoms:</h3>
+							<ul>
+								<li>Shortness of breath</li>
+								<li>Loss of appetite</li>
+								<li>Confusion</li>
+								<li>
+									Persistent pain or pressure in the chest
+								</li>
+								<li>High temperature (above 38°C)</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
